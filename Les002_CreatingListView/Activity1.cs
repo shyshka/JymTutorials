4587744168da8ae294fe64a55624b83f2319d765
Oleg Shyshka <shyshka.oleg@gmail.com>
2015-06-04 09:26:40 +0300

    Create solution Add lesson 01 Add lesson 02
    Signed-off-by: Oleg Shyshka <shyshka.oleg@gmail.com>

---
+++ Les002_CreatingListView/Activity1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace Les002_CreatingListView
+{
+    [Activity(Label = "Les002_CreatingListView", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Activity1 : Activity
+    {
+        private List<string> mItems;
+        private ListView mListView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            mListView = FindViewById<ListView>(Resource.Id.myListView);
+
+            mItems = new List<string>();
+            mItems.Add("Bob");
+            mItems.Add("Tom");
+            mItems.Add("Jym");
+
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, mItems);
+            mListView.Adapter = adapter;
+        }
+    }
+}
+
